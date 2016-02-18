7c273af068be72ce3757f20dcdaab7065dc5df80
Daisy Molving <daisymolving@gmail.com>
2016-02-18 15:47:00 +0000

    wrote secondary test for find_length method, test fails

--- bubblesort_spec.rb
+++ bubblesort_spec.rb
@@ -7,4 +7,10 @@ describe Bubblesort do
 	 result = bubblesort.find_length([1,2,3])
 	 expect(result).to eq 3
   end
+
+  it 'finds the length of array [1,7,8,9,6]' do
+	 bubblesort = Bubblesort.new
+	 result = bubblesort.find_length([1,7,8,9,6])
+	 expect(result).to eq 5
+  end
 end
