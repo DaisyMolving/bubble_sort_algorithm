a9872d17e40fabe490db2fbafb15788588c603bb
Daisy Molving <daisymolving@gmail.com>
2016-02-18 16:38:18 +0000

    sorted array needs work, moving on to next test

--- bubblesort.rb
+++ bubblesort.rb
@@ -5,8 +5,16 @@ class Bubblesort
 	bubblength
   end
 
-  def sorted(array_to_sort)
-
-	 true
-  end
+  # def sorted(array_to_sort)
+	 # truly_sorted = array_to_sort.each do |num|
+		# order = ((num <=> num.next)  == -1)
+		# puts order
+		# if order
+		  # true
+		# else
+		  # false
+		# end
+	# end
+	# truly_sorted
+  # end
 end
