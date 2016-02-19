571822c045383dc670d665d8cb3981ac5b705c85
Daisy Molving <daisymolving@gmail.com>
2016-02-19 09:38:07 +0000

    wrote method called order that takes array_to_sort and loops through it, checking each element against the next element (unless the current element is the last in array) and swaps them if current is larger than next

--- bubblesort.rb
+++ bubblesort.rb
@@ -5,25 +5,30 @@ class Bubblesort
 	bubblength
   end
 
-  # def sorted(array_to_sort)
-	 # truly_sorted = array_to_sort.each do |num|
-		# order = ((num <=> num.next)  == -1)
-		# puts order
-		# if order
-		  # true
-		# else
-		  # false
+
+  # def order(array_to_sort)
+	 # array_to_sort.each_cons(2) do |num, next_num|  
+		# if num > next_num
+			# current_order = num, next_num 
+			# current_order.reverse!
 		# end
-	# end
-	# truly_sorted
+		# current_order
+	 # end
+	 # array_to_sort.insert(0, current_order).pop(2)
+	 # array_to_sort.flatten!
   # end
 
-  def swap!(array_to_sort)
-	 array_to_sort[0], array_to_sort[1] = array_to_sort[1], array_to_sort[0]
-	 array_to_sort
-  end
-
   def order(array_to_sort)
-	 swap!(array_to_sort)
+	 current_index = 0
+	 while current_index < array_to_sort.length
+		next_index = current_index + 1
+		unless  array_to_sort.at(current_index) == array_to_sort[-1]	
+		  if array_to_sort.at(current_index) > array_to_sort.at(next_index)
+		  array_to_sort.insert(current_index, array_to_sort.delete_at(current_index + 1))
+		  end
+		end
+		current_index += 1
+	 end
+	 array_to_sort
   end
 end
--- bubblesort_spec.rb
+++ bubblesort_spec.rb
@@ -14,30 +14,6 @@ describe Bubblesort do
 	 expect(result).to eq 5
   end
 
-  # it 'should return true if array is sorted' do
-	 # bubblesort = Bubblesort.new
-	 # result = bubblesort.sorted([1,2,3,7,8])
-	 # expect(result).to eq true
-  # end
-
-  # it 'should return false if array is not sorted' do
-	 # bubblesort = Bubblesort.new
-	 # result = bubblesort.sorted([6,3,1,4,7])
-	 # expect(result).to eq false
-  # end
-
-  it 'should swap numbers in array [5,3] to be in ascending order' do
-	 bubblesort = Bubblesort.new
-	 result = bubblesort.swap!([5,3])
-	 expect(result).to eq [3,5]
-  end
-
-  it 'should swap numbers in array [7,2] to be in ascending order' do
-	 bubblesort = Bubblesort.new
-	 result = bubblesort.swap!([7,2])
-	 expect(result).to eq [2,7]
-  end
-
   it 'should swap numbers in array if not in ascending order' do
 	 bubblesort = Bubblesort.new
 	 result = bubblesort.order([7,2])
